b49a55ec3634c1712a0229b38d1c89668e33a271
handyman0 <118775048+handyman0@users.noreply.github.com>
2023-10-25 21:59:21 -0300

    Update GeraBackupHome.sh
    deixando o codigo limpo e legivel

--- GeraBackupHome.sh
+++ GeraBackupHome.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
+#####################################################################
+# Nome: GeraBackupHome.sh                                           #
+#                                                                   #
+# Autor: Luiz Fernando (www.github.com.br/handyman0)                #
+# Data: 25/10/2023                                                  #
+#                                                                   #
+# Descrição: O script fara um backup compactado do diretorio home   #
+#		do usuario que estiver executando o script.         #
+# Uso: ./GeraBackupHome.sh                                          #
+#                                                                   #
+# v0.1                                                              #
+#                                                                   #
+# 25/10/2023 - luiz fernando - criando o algoritmo de copia         #
+#                                                                   #
+#####################################################################
+
+# Principais Variaveis
 PROCURADIR=$(find /home/lfms/ -type d -name "Backup")
 DATADEHOJE=$(date +%Y%m%d%H%M)
 ULTIMODIR=$(ls "$PROCURADIR" -t1 | head -n 1)
+
+# procurando se tem a variavel no depois do diretorio do usuario
 if test -d "$PROCURADIR";
 then
 	cd "$PROCURADIR"
-	if test -d $ULTIMODIR; then 
+	if test -d $ULTIMODIR;
+	then    # criando as variaveis para facilitar as contagens do dias
 		DATACRIACAO=$(stat -c %Y "$ULTIMODIR")
 		DATAATUAL=$(date +%s)
 		DIFERENCA=$((DATAATUAL - DATACRIACAO))
 		SETEDIAS=$((7 * 24 * 60 * 60))
+		# Esse if é para fazer a condição se foi feito o backup antes ou depois de 7 dias
 		if test "$DIFERENCA" -le "$SETEDIAS"; then
 			echo "Ja foi criado um diretorio nos ultimos 7 dias"
 			read -p "Deseja Continuar? (S/N)" RESPOSTA
 			echo ""
+			# Melhor opção ate o momento de fazer case de questionamento de como prosseguir
 			case "$RESPOSTA" in
 				y|s|S)
 					echo "Será criado mais um backup para a mesma semana."
@@ -29,7 +51,8 @@ then
 					echo "Backup Concluído!"
 					;;
 
-				n|N)
+				n|N)    # Se o usuario não quiser prosseguir com o backup adicional 
+                                        #coloquei como saida direto
 					echo "saindo .."
 					exit 0
 					;;
@@ -38,7 +61,7 @@ then
 					exit 1
 					;;
 			esac
-		else
+		else    #saindo da condição criada :D  aqui ja indo para o backup direto
 			echo "Criando um diretorio!"
 			echo ""
 			mkdir backup_home_$DATADEHOJE
